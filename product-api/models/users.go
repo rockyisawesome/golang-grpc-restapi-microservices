81b7207355b9f379cb0ebca5be2d720def6277b7
Chandransh pandey <pandeychandransh@gmail.com>
2025-02-15 16:27:00 +0530

    fixed MongoDb ID issue we need to use v2 mongo-driver and change primitive.ObjectId to bson.ObjectID

--- product-api/models/users.go
+++ product-api/models/users.go
@@ -1,5 +1,11 @@
 package models
 
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/v2/bson"
+)
+
 // type Users struct {
 // 	ID        primitive.ObjectID `bson: "_id""`
 // 	Name      string             `bson:"name"`
@@ -7,14 +13,15 @@ package models
 // 	CreatedAt time.Time          `bson:"created_at"`
 // }
 
-// type Users struct {
-// 	ID        primitive.ObjectID `bson:"id"`
-// 	Name      string             `bson:"name"`
-// 	Email     string             `bson:"email"`
-// 	CreatedAt time.Time          `bson:"created_at"`
-// }
-
 type Users struct {
-	Name  string `bson:"name"`
-	Email string `bson:"email"`
+	ID        bson.ObjectID `bson:"_id" json:"id"`
+	Name      string        `bson:"name" json:"name"`
+	Email     string        `bson:"email" json:"email"`
+	CreatedAt time.Time     `bson:"created_at" json:"-"`
+}
+
+type UserAPI struct {
+	ID    string `json:"_id"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
 }
